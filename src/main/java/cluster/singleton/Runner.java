c18e53abc253d5be206aa0d5321dcd4faf9dd606
Hugh McKee <mckee.hugh@gmail.com>
2018-04-23 19:10:51 -0400

    Small code format adjustment

--- src/main/java/cluster/singleton/Runner.java
+++ src/main/java/cluster/singleton/Runner.java
@@ -57,8 +57,7 @@ public class Runner {
         return ConfigFactory.parseString(
                 String.format("akka.remote.netty.tcp.port=%s%n", port) +
                         String.format("akka.remote.artery.canonical.port=%s%n", port))
-                .withFallback(ConfigFactory.load()
-                );
+                .withFallback(ConfigFactory.load());
     }
 
     private static void createClusterSingletonManagerActor(ActorSystem actorSystem) {
